9af1dcb44bf86fb1224ac11e1c0f6f2633f1d3ff
Mark Cooper <mark@liquidjelly.co.uk>
2019-01-14 15:03:47 +0000

    Added environment variables to the page to check status of development vs deployment

---
+++ CamperFinder.Web/Models/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CamperFinder.Web.Models
+{
+    public class Config
+    {
+        public string ApiUrl { get; set; }
+    }
+}
--- CamperFinder.Web/Pages/Index.cshtml.cs
+++ CamperFinder.Web/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,7 +12,8 @@ namespace CamperFinder.Web.Pages
     {
         public void OnGet()
         {
-
+            HttpClient _client = new HttpClient();
+            
         }
     }
 }
